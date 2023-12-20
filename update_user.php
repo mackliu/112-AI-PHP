5ec5611673faa51f36628383e6507dd1686863b8
AhwuIOT <ahwuiot@gmail.com>
2023-12-20 15:18:49 +0800

    在upadet_user.php 中完成更新乘客功能

--- update_user.php
+++ update_user.php
@@ -1,5 +1,23 @@
 <?php include_once "db.php";
 
-dd($_POST);
+//dd($_POST);
 
+
+$sql="update `titanic` set 
+             `Survived`='{$_POST['Survived']}' ,
+             `Pclass`='{$_POST['Pclass']}' ,
+             `Name`='{$_POST['Name']}' ,
+             `Sex`='{$_POST['Sex']}' ,
+             `Age`='{$_POST['Age']}' ,
+             `SibSp`='{$_POST['SibSp']}' ,
+             `Parch`='{$_POST['Parch']}' ,
+             `Ticket`='{$_POST['Ticket']}' ,
+             `Fare`='{$_POST['Fare']}' ,
+             `Cabin`='{$_POST['Cabin']}' ,
+             `Embarked`='{$_POST['Embarked']}' 
+where `PassengerId`='{$_POST['PassengerId']}'";
+
+$pdo->exec($sql);
+
+header("location:index.php");
 ?>
