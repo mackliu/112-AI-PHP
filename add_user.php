6327040fd1896592d4e1a10a1d812e22f947c5b0
mackliu <macklun@gmail.comm>
2023-12-21 00:44:52 +0800

    1. 增加年齡長條圖 2. 增加顯示全部資料 3. 美化畫面 4. 美化新增表單畫面

--- add_user.php
+++ add_user.php
@@ -1,52 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>新增使用者</title>
+    <!-- 引入 Bootstrap 5.0 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+<body>
+<main class="container">
 
 <h1>新增使用者</h1>
-<form action="insert_user.php" method="post">
-    <div>
-        <label for="Survived">Survived</label>
-        <input type="text" name="Survived" id="Survived" >
-    </div>
-    <div>
-        <label for="Pclass">Pclass</label>
-        <input type="text" name="Pclass" id="Pclass" >
-    </div>
-    <div>
-        <label for="Name">Name</label>
-        <input type="text" name="Name" id="Name" >
-    </div>
-    <div>
-        <label for="Sex">Sex</label>
-        <input type="text" name="Sex" id="Sex" >
-    </div>
-    <div>
-        <label for="Age">Age</label>
-        <input type="text" name="Age" id="Age" >
-    </div>
-    <div>
-        <label for="SibSp">SibSp</label>
-        <input type="text" name="SibSp" id="SibSp">
-    </div>
-    <div>
-        <label for="Parch">Parch</label>
-        <input type="text" name="Parch" id="Parch" >
-    </div>
-    <div>
-        <label for="Ticket">Ticket</label>
-        <input type="text" name="Ticket" id="Ticket" >
-    </div>
-    <div>
-        <label for="Fare">Fare</label>
-        <input type="text" name="Fare" id="Fare" >
-    </div>
-    <div>
-        <label for="Cabin">Cabin</label>
-        <input type="text" name="Cabin" id="Cabin">
-    </div>
-    <div>
-        <label for="Embarked">Embarked</label>
-        <input type="text" name="Embarked" id="Embarked" >
-    </div>
-    <div>
-        <input type="submit" value="確認編輯">
-        <input type="reset" value="重置">
-    </div>
-</form>
+    <a class="btn btn-success" href="index.php">回到首頁</a>
+    <form action="insert_user.php" method="post" class="col-6 m-auto">
+        <div class="form-group">
+            <label for="Survived">Survived</label>
+            <select class="form-control" name="Survived" id="Survived">
+                <option value="1">生存</option>
+                <option value="0">死亡</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="Pclass">Pclass</label>
+            <select class="form-control" name="Pclass" id="Pclass">
+                <option value="1">頭等艙</option>
+                <option value="2">豪華艙</option>
+                <option value="3">經濟艙</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="Name">Name</label>
+            <input type="text" class="form-control" name="Name" id="Name" >
+        </div>
+        <div class="form-group">
+            <label for="Sex">Sex</label>
+            <div class="d-flex">
+                <div class="form-check me-4">
+                    <input class="form-check-input" type="radio" name="Sex" id="male" value="male">
+                    <label class="form-check-label" for="male">
+                        男性
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="Sex" id="female" value="female">
+                    <label class="form-check-label" for="female">
+                        女性
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="Age">Age</label>
+            <input type="number" step="0.1" class="form-control" name="Age" id="Age" >
+        </div>
+        <div class="form-group">
+            <label for="SibSp">SibSp</label>
+            <input type="number" class="form-control" name="SibSp" id="SibSp" >
+        </div>
+        <div class="form-group">
+            <label for="Parch">Parch</label>
+            <input type="number" class="form-control" name="Parch" id="Parch" >
+        </div>
+        <div class="form-group">
+            <label for="Ticket">Ticket</label>
+            <input type="text" class="form-control" name="Ticket" id="Ticket" >
+        </div>
+        <div class="form-group">
+            <label for="Fare">Fare</label>
+            <input type="test"  class="form-control" name="Fare" id="Fare" >
+        </div>
+        <div class="form-group">
+            <label for="Cabin">Cabin</label>
+            <input type="text" class="form-control" name="Cabin" id="Cabin" >
+        </div>
+        <div class="form-group">
+            <label for="Embarked">Embarked</label>
+            <select class="form-control" name="Embarked" id="Embarked">
+                <option value="C">Cherbourg</option>
+                <option value="S">Southampton</option>
+                <option value="Q">Queenstown</option>
+            </select>
+        </div>
+        <div class="form-group my-4">
+            <button type="submit" class="btn btn-primary">新增</button>
+            <input type="reset" value="清空">
+        </div>
+    </form>
+</main>
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
 <!--標頭區-->   
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner" style="height:45vh">
+  <div class="carousel-inner" style="height:35vh;box-shadow:0 0 15px black">
     <div class="carousel-item active">
       <img src="./img/banner.jpg" class="d-block w-100" alt="...">
     </div>
@@ -18,9 +18,18 @@
 </div>
 <!--主內容區-->   
 <main class="container">
+
+<!--統計區-->
 <div class='rounded row border my-4 p-4'>
 <div class='col-4'>
   <?php
+    /**
+     * 1.取得總筆數
+     * 2.取得男生筆數
+     * 3.取得女生筆數
+     * 4.計算男女比例
+     * 5.顯示男女比例
+     */
     $row_total=$pdo->query("select count(*) from `titanic`")->fetchColumn();
     $males=$pdo->query("SELECT count(*) FROM `titanic` where `Sex`='male';")->fetchColumn();
     $males_rate=round($males/$row_total,4);
@@ -33,6 +42,14 @@
 </div>
 <div  class='col-4'>
 <?php
+    /**
+     * 1.取得總筆數
+     * 2.取得艙等1筆數
+     * 3.取得艙等2筆數
+     * 4.取得艙等3筆數
+     * 5.計算艙等比例
+     * 6.顯示艙等比例
+     */
     $class1=$pdo->query("SELECT count(*) FROM `titanic` where `Pclass`='1';")->fetchColumn();
     $class1_rate=round($class1/$row_total,4);
     $class2=$pdo->query("SELECT count(*) FROM `titanic` where `Pclass`='2';")->fetchColumn();
@@ -56,78 +73,96 @@
 </div>
 <div  class='col-4'>
 <?php
-    
+    /**
+     * 1.取得總筆數
+     * 2.取得小孩筆數
+     * 3.取得青少年筆數
+     * 4.取得成人筆數
+     * 5.取得中高年筆數
+     * 6.取得高齡筆數
+     * 7.取得不詳筆數
+     * 8.計算年紀比例
+     * 9.顯示年紀比例
+     */
     $age1=$pdo->query("SELECT count(*) FROM `titanic` where `Age`>0 && `Age`<16;")->fetchColumn();
+    $age1_rate=round($age1/$row_total,4);
     $age2=$pdo->query("SELECT count(*) FROM `titanic` where `Age`>=16 && `Age`<20;")->fetchColumn();
+    $age2_rate=round($age2/$row_total,4);
     $age3=$pdo->query("SELECT count(*) FROM `titanic` where `Age`>=20 && `Age`<45;")->fetchColumn(); 
+    $age3_rate=round($age3/$row_total,4);
     $age4=$pdo->query("SELECT count(*) FROM `titanic` where `Age`>=45 && `Age`<60;")->fetchColumn(); 
+    $age4_rate=round($age4/$row_total,4);
     $age5=$pdo->query("SELECT count(*) FROM `titanic` where `Age`>=60;")->fetchColumn(); 
+    $age5_rate=round($age5/$row_total,4);
     $age6=$pdo->query("SELECT count(*) FROM `titanic` where `Age`<=0")->fetchColumn();
+    $age6_rate=round($age6/$row_total,4);
   ?>
-  <p>年紀:</p>
-  <p>小孩:<?=$age1;?>(<?=round($age1/$row_total,4)*100;?>%)</p>
-  <p>青少年:<?=$age2;?>(<?=round($age2/$row_total,4)*100;?>%)</p>
-  <p>成人:<?=$age3;?>(<?=round($age3/$row_total,4)*100;?>%)</p>
-  <p>中高齡:<?=$age4;?>(<?=round($age4/$row_total,4)*100;?>%)</p>
-  <p>高齡:<?=$age5;?>(<?=round($age5/$row_total,4)*100;?>%)</p>
-  <p>不詳:<?=$age6;?>(<?=round($age6/$row_total,4)*100;?>%)</p>
+  <div>年紀:</div>
+  <div>
+    <div class="d-inline-block" style="width:60px">小孩:</div> 
+    <div class='line d-inline-block bg-info align-middle' style="height:20px;width:<?=280*$age1_rate;?>px"></div> 
+    <?=$age1;?>(<?=round($age1/$row_total,4)*100;?>%)</div>
+  <div>
+    <div class="d-inline-block" style="width:60px">青少年:</div>
+    <div class='line d-inline-block bg-info align-middle' style="height:20px;width:<?=280*$age2_rate;?>px"></div>
+    <?=$age2;?>(<?=round($age2/$row_total,4)*100;?>%)</div>
+  <div>
+    <div class="d-inline-block" style="width:60px">成人:</div>
+    <div class='line d-inline-block bg-info align-middle' style="height:20px;width:<?=280*$age3_rate;?>px"></div>
+    <?=$age3;?>(<?=round($age3/$row_total,4)*100;?>%)</div>
+  <div>
+    <div class="d-inline-block" style="width:60px">中高:</div>
+    <div class='line d-inline-block bg-info align-middle' style="height:20px;width:<?=280*$age4_rate;?>px"></div>
+    <?=$age4;?>(<?=round($age4/$row_total,4)*100;?>%)</div>
+  <div>
+    <div class="d-inline-block" style="width:60px">高齡:</div>
+    <div class='line d-inline-block bg-info align-middle' style="height:20px;width:<?=280*$age5_rate;?>px"></div>
+    <?=$age5;?>(<?=round($age5/$row_total,4)*100;?>%)</div>
+  <div>
+    <div class="d-inline-block" style="width:60px">不詳:</div>
+    <div class='line d-inline-block bg-info align-middle' style="height:20px;width:<?=280*$age6_rate;?>px"></div>
+    <?=$age6;?>(<?=round($age6/$row_total,4)*100;?>%)</div>
 </div>
-
 </div>
-
-
-
 <?php
+
+/**
+ * 根據網址參數決定要查詢的資料
+ */
 if(isset($_GET['type'])){
   $total=$pdo->query("select count(*) from titanic where `{$_GET['type']}`='{$_GET['v']}'")->fetchColumn();
 }else{
   $total=$pdo->query("select count(*) from titanic")->fetchColumn();
 }
 
+/**
+ * 分頁功能所需參數
+ */
 $div=50;
 $pages=ceil($total/$div);
 $now=$_GET['p']??1;
-/* $now=(isset($_GET['p']))?$_GET['p']:1; */
-/* if(isset($_GET['p'])){
-  $now=$_GET['p'];
-}else{
-  $now=1
-} */
 $start=($now-1)*$div;
 if(isset($_GET['type'])){
   $sql="select * from `titanic` where `{$_GET['type']}`='{$_GET['v']}'";
 }else{
-
   $sql="select * from `titanic` limit $start,$div";
 }
 $users=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<div class="statistics">
-  <!--統計區-->
-</div>
 <div >
   <a href="add_user.php" class="btn btn-warning float-end">新增</a>
 </div>
-<div class="filter d-flex w-100">
+<!--篩選區-->
+<div class="filter d-flex w-100 my-3 border-bottom pb-2">
+  <a href="?"  class='btn btn-success mx-1'>全部</a>
   <div class='btn btn-info mx-1'>性別:</div>
-  <div class='btn btn-primary mx-1'>
-    <a href="?type=Sex&v=male" class='text-white'>男</a>
-  </div>
-  <div class='btn btn-primary mx-1'>
-    <a href="?type=Sex&v=female" class='text-white'>女</a>
-  </div>
+    <a href="?type=Sex&v=male"   class='btn btn-primary mx-1'>男</a>
+    <a href="?type=Sex&v=female" class='btn btn-primary mx-1'>女</a>
   <div class='btn btn-info ms-3'>艙等:</div>
-  <div class='btn btn-primary mx-1'>
-    <a href='?type=Pclass&v=3' class='text-white'>普通</a>
-  </div>
-  <div class='btn btn-primary mx-1'>
-    <a href='?type=Pclass&v=2' class='text-white'>經濟</a>
-  </div>
-  <div class='btn btn-primary mx-1'>
-    <a href='?type=Pclass&v=1' class='text-white'>特等</a>
-  </div>
-
+    <a href='?type=Pclass&v=3' class='btn btn-primary mx-1'>普通</a>
+    <a href='?type=Pclass&v=2' class='btn btn-primary mx-1'>經濟</a>
+    <a href='?type=Pclass&v=1' class='btn btn-primary mx-1'>特等</a>
 </div>
 <div class='d-flex justify-content-between'>
   <div>
@@ -144,8 +179,6 @@ $users=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
       echo "<a href='' class='d-inline-block btn btn-sm btn-primary px-2 py-1 m-1'> <img src='./img/arrow-left-solid.svg'> </a>";
     }
     ?>
-    
-
   </div>
   <div>
   <?php 
@@ -172,13 +205,12 @@ $users=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 </div>
 <div class="row">
 <?php 
-
-
 foreach($users as $user){
   $bg=($user['Survived']==1)?'lightgreen':'#DDD';
   $nameBold=($user['Survived']==1)?"font-weight:bolder":"font-weight:300";
 ?>
-<div class="card m-3 " style="width: 18rem;box-shadow:2px 2px 5px #CCC ;background-color:<?=$bg;?>">
+<div class="col-3 my-3 d-flex justify-content-center items-center">
+<div class="card" style="width: 18rem;box-shadow:2px 2px 5px #CCC ;background-color:<?=$bg;?>">
 
   <div class="card-body position-relative" style="height:240px">
     <h5 class="card-title" style="<?=$nameBold;?>"><?=$user['Name'];?></h5>
@@ -193,11 +225,12 @@ foreach($users as $user){
     </p>
     <p class='card-text'>Sex:<?=$user['Sex'];?></p>
     <p class='card-text'>Pclass:<?=$user['Pclass'];?></p>
-    <div class="position-absolute text-end pe-3" style="bottom:20px; width:100%">
+    <div class="position-absolute text-end pe-3" style="bottom:20px; right:10px">
       <a href="edit_user.php?id=<?=$user['PassengerId'];?>" class="btn btn-primary">編輯</a>
       <a  class="btn btn-danger" onclick="del(<?=$user['PassengerId'];?>)">刪除</a>
     </div>
   </div>
+</div>
 </div>
 <?php 
 }
