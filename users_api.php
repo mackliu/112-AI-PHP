ccd630a10a7718f6f46e1d60be57ece8690cc44b
AhwuIOT <ahwuiot@gmail.com>
2023-12-21 13:08:06 +0800

    限制資料表輸出到前端的內容以縮小傳輸流量

--- users_api.php
+++ users_api.php
@@ -1,7 +1,7 @@
 <?php
 include_once "db.php";
 
-$sql="select * from `titanic` limit 50";
+$sql="select `Survived`,`Name`,`Pclass`,`Age`,`PassengerId`,`Sex` from `titanic` limit 50";
 $users=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
 header('Content-Type: application/json; charset=utf-8');
